e5422db6f14149273d059c175faab32d3735f0ff
MarceloDoehnert <59656981+MarceloDoehnert@users.noreply.github.com>
2023-01-29 22:17:20 -0300

    header and routes done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
-export function App() {
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+import { Router } from './Router'
+
+export function App() {
   return (
-    <h1>Hello World</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
