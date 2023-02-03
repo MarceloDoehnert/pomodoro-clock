6da8b0035722082867922242dc0c133838db24ec
MarceloDoehnert <59656981+MarceloDoehnert@users.noreply.github.com>
2023-02-03 19:29:00 -0300

    history page styles

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,72 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>
+                <Status statusColors="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>
+                <Status statusColors="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>
+                <Status statusColors="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>
+                <Status statusColors="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>
+                <Status statusColors="yellow">Em andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>há 2 meses</td>
+              <td>
+                <Status statusColors="red">Interrompido</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
